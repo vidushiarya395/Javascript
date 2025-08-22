62738415088db5192a26b7a41e8240ccfc2de877
vidushiarya395 <vidushibaliyan395@gmail.com>
2025-08-22 11:42:51 +0000

    container config done

---
+++ 01_basics/07_stringToNumber.js
@@ -0,0 +1,41 @@
+let value =3
+let negvalue =-value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " viddu"
+let str3 = str1+ str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+3);//123
+console.log(1+2+"3");//33
+
+console.log(+true);
+console.log(+"");
+//console.log(true+); // error
+
+let x = 2
+const y = ++x //also try x++
+console.table([x,y]);
+
+
+
+
+
+
+
+
+
+
+
+
