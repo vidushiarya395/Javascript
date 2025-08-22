42693e22154eafd6be1a8777c0428a179f802fae
vidushiarya395 <vidushibaliyan395@gmail.com>
2025-08-22 10:26:49 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,75 @@
+"use strict"; // treat all js code as newer version
+
+let name = "vidushi"
+let age = 16
+let signinStatus = false
+
+// basic data types are:-
+// number> within some range of 2 raised to the power
+// big int> when value is too large
+// String> ""
+// boolean > true,false
+// null> standalone value
+// undefined
+// symbol> unique
+
+
+//object
+
+
+console.log(typeof "khushboo")
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof signinStatus)
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+
+
+
+
+
+
+//alert(3+3) //shows error as we are using node js not browser
+
+
+
+
+
+/* ECMAScript (ES)
+
+ECMAScript (ES) is the official standard on which JavaScript is based.
+
+Think of it like this:
+
+ECMAScript = the rulebook (specification).
+
+JavaScript = the actual language you use in browsers, based on that rulebook.
+
+Example:
+
+ES5, ES6, ES7 … are different versions of ECMAScript.
+
+ES6 (released in 2015) introduced many new features in JavaScript, like:
+
+let and const
+
+
+So whenever you hear ES6, ES7, ES2022, that’s just the ECMAScript version being referred to.
+
+📚 MDN (Mozilla Developer Network)
+
+MDN Web Docs is a documentation website created by Mozilla.
+
+It’s the most trusted source for web technologies (JavaScript, HTML, CSS, APIs, etc.).
+
+Developers use it to learn how JavaScript functions, objects, and methods work, with examples and explanations.
+
+👉 Example: If you search “JavaScript Array MDN”, you’ll get the full guide for arrays on MDN:
+
+Syntax
+
+Examples
+
+Browser compatibility
+
+Best practices */
